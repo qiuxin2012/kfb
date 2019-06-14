131f8f475a219e4eef4bc2e0dcd25adcc0cc25f3
Jiaming Song <litchilitchy@qq.com>
2019-06-14 13:37:16 +0800

    Add files via upload

--- py/monitor.py
+++ py/monitor.py
@@ -9,17 +9,29 @@ pos_neg_threshold = 0.5
 kfb_pos_ratio = 0.01
 
 
-def prob_to_pos(rec):
+def prob_to_pos(line):
+    # print(rec)
+    # assert len(rec) == 3
+    print(line)
+    rec = line.split("|")
+    prob = float(rec[1])
     assert len(rec) == 3
-    if rec[1] >= pos_neg_threshold:
+    if prob <= pos_neg_threshold:
         return rec[0], 1, 1
     else:
         return rec[0], 0, 1
 
 
 def count_pos(rec1, rec2):
+    kfb1, kfb2 = rec1[0].split("/")[0], rec2[0].split("/")[0]
+
     assert len(rec1) == 3 and len(rec2) == 3
-    assert rec1[0] == rec2[0]
+    assert kfb1 == kfb2
+    # if rec1[0] != rec2[0]:
+    #     print(rec1[0], rec2[0])
+    #     time.sleep(60)
+    #     assert rec1[0] == rec2[0]
+
     return rec1[0], rec1[1] + rec2[1], rec1[2] + rec2[2]
 
 
@@ -30,10 +42,13 @@ def get_result(rec):
     '''
     res = rec.map(prob_to_pos).reduce(count_pos)
     ratio = res[1] / res[2]
+    print("positive pieces ", res[1], "  total pieces ", res[2])
+
+    kfb_name = res[0].split("/")[0]
     if ratio > kfb_pos_ratio:
-        print(res[0], " result is positive")
+        print(kfb_name, " ratio is ", ratio, " result is positive")
     else:
-        print(res[0], " result is negative")
+        print(kfb_name, " ratio is ", ratio, " result is negative")
     return
 
 
@@ -47,6 +62,8 @@ if __name__ == "__main__":
     # print (a.count())
     #
     # b = get_result(a)
+    log4jLogger = sc._jvm.org.apache.log4j
+    LOGGER = log4jLogger.LogManager.getLogger(__name__)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_path', help="text file path, usually hdfs://")
@@ -65,14 +82,17 @@ if __name__ == "__main__":
     while 1:
         while DB.llen('count-kfb') > 0:
 
-            rec = DB.lpop('count-kfb').decode().split("|")
-            # print(rec.decode())
-            fname, total_count = rec[0], rec[1]
+            kv = DB.lpop('count-kfb').decode().split("|")
 
+            # print(rec.decode())
+            fname, total_count = kv[0], kv[1]
+            DB.lpush('count-kfb', (fname + "|" + total_count))
+            total_count = int(total_count)
+
+            dir_path = os.path.join(file_path, fname)
             print("file name is -> ", fname, "total_count is -> ", total_count)
 
-            file_path = os.path.join(file_path, fname)
-            cnt_rdd = sc.textFile(file_path)
+            cnt_rdd = sc.textFile(dir_path)
             current_count = cnt_rdd.count()
             print("current cnt is -> ", current_count, "   total cnt is -> ", total_count)
             if total_count == current_count:
@@ -80,9 +100,13 @@ if __name__ == "__main__":
                 btime = time.time()
                 get_result(cnt_rdd)
                 print("reduce time elapsed ", time.time() - btime)
+                DB.lpop('count-kfb')
             else:
-                DB.lpush('count-kfb', (fname + "|" + total_count))
-            time.sleep(5)
+                print("write not ends")
+                print(type(total_count), type(current_count))
+
+
+            time.sleep(30)
         print("queue is empty")
         time.sleep(60)
 
